4ae46fb6f346c6656ec1ebe82662e30609e1a727
Alexandra <alexankitty@gmail.com>
2025-01-11 14:43:56 -0700

    add if check. Add proton version via umu

--- AemulusSetupProton.sh
+++ AemulusSetupProton.sh
@@ -1,37 +1,39 @@
 #!/bin/sh
 #Environment
 WINEDLLOVERRIDES="mscoree=d;mshtml=d"
+#required for umu to work
+export GAMEID="aemulus"
 export WINEPREFIX=$HOME/.local/share/aemulus
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ICON_PATH=~/.local/share/icons/hicolor/256x256/apps/
-EXEC_LINE="Exec=env WINEPREFIX=$WINEPREFIX "
-EXEC_LINE+="DOTNET_ROOT= $HOME/.local/share/wine.AppImage wine "
+EXEC_LINE="Exec=env WINEPREFIX=$WINEPREFIX GAMEID=$GAMEID "
+EXEC_LINE+="DOTNET_ROOT= $HOME/.local/share/umu/umu-run "
 EXEC_LINE+='C:\\\\\\\\AemulusPackageManager\\\\\\\\AemulusPackageManager.exe %u'
 PATH_LINE="Path=${WINEPREFIX}/drive_c/AemulusPackageManager"
 DESKTOP_PATH="$HOME/.local/share/applications"
-alias wine='$HOME/.local/share/wine.AppImage wine'
-alias winetricks='$HOME/.local/share/wine.AppImage winetricks'
-alias wineboot='$HOME/.local/share/wine.AppImage wineboot'
+alias wine='$HOME/.local/share/umu/umu-run'
+alias winetricks='$HOME/.local/share/umu/umu-run winetricks'
 
 #Download Paths
 vcx64='https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe'
 vcx86='https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe'
 version=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/TekkaGB/AemulusModManager/releases/latest)
 version=${version##*/}
-wineimage=$(curl -sL https://api.github.com/repos/mmtrt/WINE_AppImage/releases/tags/continuous-staging | jq -r ".assets[].browser_download_url" | grep AppImage | head -1)
+umulauncher=$(curl -sL https://api.github.com/repos/Open-Wine-Components/umu-launcher/releases/latest | jq -r ".assets[].browser_download_url" | grep Zipapp.zip )
 aemulus="https://github.com/TekkaGB/AemulusModManager/releases/latest/download/AemulusPackageManagerv${version}.7z"
 dotnet8="https://download.visualstudio.microsoft.com/download/pr/84ba33d4-4407-4572-9bfa-414d26e7c67c/bb81f8c9e6c9ee1ca547396f6e71b65f/windowsdesktop-runtime-8.0.2-win-x64.exe"
 dotnet7="https://download.visualstudio.microsoft.com/download/pr/c81fc3af-c371-4bb5-a59d-fa3e852799c7/056ac9df87d92b75cc463cb106ef3b64/windowsdesktop-runtime-7.0.17-win-x64.exe"
 
-if [ ! -f $HOME/.local/share/wine.AppImage ]; then
-    curl -o $HOME/.local/share/wine.AppImage -L $wineimage
-    chmod +x $HOME/.local/share/wine.AppImage
+if [ ! -f $HOME/.local/share/umu/umu-run ]; then
+    curl -o /tmp/Zipapp.zip -L $umulauncher
+    unzip /tmp/Zipapp.zip -d /tmp
+    mkdir $HOME/.local/share/umu
+    tar -xf /tmp/Zipapp.tar -C $HOME/.local/share/umu
+    rm /tmp/Zipapp.*
 fi
-wine_ver=$(wine --version)
+wine_ver=$(wine --version | grep wine)
 wine_ver=${wine_ver#*-}
 #setup Prefix
-wineboot
-cd $SCRIPT_DIR
 curl -o /tmp/windowsdesktop-runtime-8.0.2-win-x64.exe $dotnet8
 curl -o /tmp/windowsdesktop-runtime-7.0.17-win-x64.exe $dotnet7
 curl -o /tmp/vcredist_x86.exe $vcx86
