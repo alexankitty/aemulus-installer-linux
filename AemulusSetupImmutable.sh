040bed1acba676b2f02b7780649ce208c3cc9af8
Alexandra <alexankitty@gmail.com>
2025-07-09 20:52:52 -0600

    Quote paths, should fix #6

--- AemulusSetupImmutable.sh
+++ AemulusSetupImmutable.sh
@@ -24,8 +24,8 @@ dotnet8="https://download.visualstudio.microsoft.com/download/pr/84ba33d4-4407-4
 dotnet7="https://download.visualstudio.microsoft.com/download/pr/c81fc3af-c371-4bb5-a59d-fa3e852799c7/056ac9df87d92b75cc463cb106ef3b64/windowsdesktop-runtime-7.0.17-win-x64.exe"
 
 if [ ! -f $HOME/.local/share/wine.AppImage ]; then
-    curl -o $HOME/.local/share/wine.AppImage -L $wineimage
-    chmod +x $HOME/.local/share/wine.AppImage
+    curl -o "$HOME/.local/share/wine.AppImage" -L $wineimage
+    chmod +x "$HOME/.local/share/wine.AppImage"
 fi
 wine_ver=$(wine --version)
 wine_ver=$(echo ${wine_ver#*-} | awk '{print $1;}')
@@ -33,30 +33,30 @@ wine_ver=$(echo ${wine_ver%.*})
 #setup Prefix
 wineboot
 cd $SCRIPT_DIR
-curl -o /tmp/windowsdesktop-runtime-8.0.2-win-x64.exe $dotnet8
-curl -o /tmp/windowsdesktop-runtime-7.0.17-win-x64.exe $dotnet7
-curl -o /tmp/vcredist_x86.exe $vcx86
-curl -o /tmp/vcredist_x64.exe $vcx64
+curl -o "/tmp/windowsdesktop-runtime-8.0.2-win-x64.exe" $dotnet8
+curl -o "/tmp/windowsdesktop-runtime-7.0.17-win-x64.exe" $dotnet7
+curl -o "/tmp/vcredist_x86.exe" $vcx86
+curl -o "/tmp/vcredist_x64.exe" $vcx64
 if [[ $wine_ver -lt 9 ]]
 then
     winetricks -q dotnet48
     winetricks -q dotnet35
     winetricks -q win10
 fi
-wine /tmp/windowsdesktop-runtime-8.0.2-win-x64.exe /passive
-wine /tmp/windowsdesktop-runtime-7.0.17-win-x64.exe /passive
-wine /tmp/vcredist_x86.exe /passive
-wine /tmp/vcredist_x64.exe /passive
-rm /tmp/vcredist_x86.exe /tmp/vcredist_x64.exe /tmp/windowsdesktop-runtime-8.0.2-win-x64.exe /tmp/windowsdesktop-runtime-7.0.17-win-x64.exe
-curl -Lso /tmp/AemulusPackageManager.7z $aemulus
-7z x /tmp/AemulusPackageManager.7z -o"${WINEPREFIX}/drive_c/"
-rm /tmp/AemulusPackageManager.7z
+wine "/tmp/windowsdesktop-runtime-8.0.2-win-x64.exe" /passive
+wine "/tmp/windowsdesktop-runtime-7.0.17-win-x64.exe" /passive
+wine "/tmp/vcredist_x86.exe" /passive
+wine "/tmp/vcredist_x64.exe" /passive
+rm "/tmp/vcredist_x86.exe" "/tmp/vcredist_x64.exe" "/tmp/windowsdesktop-runtime-8.0.2-win-x64.exe" "/tmp/windowsdesktop-runtime-7.0.17-win-x64.exe"
+curl -Lso "/tmp/AemulusPackageManager.7z" "$aemulus"
+7z x "/tmp/AemulusPackageManager.7z" -o"${WINEPREFIX}/drive_c/"
+rm "/tmp/AemulusPackageManager.7z"
 mv "${WINEPREFIX}/drive_c/AemulusPackageManagerv${version}" "${WINEPREFIX}/drive_c/AemulusPackageManager"
 
 #Setup desktop icon
-cp $SCRIPT_DIR/AemulusPackageManager.png $ICON_PATH/AemulusPackageManager.png
-sed -i "s|^Exec=.*|$EXEC_LINE|" $SCRIPT_DIR/AemulusPackageManager.desktop
-sed -i "s|^Path=.*|$PATH_LINE|" $SCRIPT_DIR/AemulusPackageManager.desktop
+cp "$SCRIPT_DIR/AemulusPackageManager.png" "$ICON_PATH/AemulusPackageManager.png"
+sed -i "s|^Exec=.*|$EXEC_LINE|" "$SCRIPT_DIR/AemulusPackageManager.desktop"
+sed -i "s|^Path=.*|$PATH_LINE|" "$SCRIPT_DIR/AemulusPackageManager.desktop"
 
-desktop-file-install --dir=$DESKTOP_PATH $SCRIPT_DIR/AemulusPackageManager.desktop
-update-desktop-database $DESKTOP_PATH
+desktop-file-install --dir=$DESKTOP_PATH "$SCRIPT_DIR/AemulusPackageManager.desktop"
+update-desktop-database "$DESKTOP_PATH"
--- AemulusSetupProton.sh
+++ AemulusSetupProton.sh
@@ -35,30 +35,30 @@ wine_ver=$(wine --version)
 wine_ver=$(echo ${wine_ver#*-} | awk '{print $1;}')
 wine_ver=$(echo ${wine_ver%.*})
 #setup Prefix
-curl -o /tmp/windowsdesktop-runtime-8.0.2-win-x64.exe $dotnet8
-curl -o /tmp/windowsdesktop-runtime-7.0.17-win-x64.exe $dotnet7
-curl -o /tmp/vcredist_x86.exe $vcx86
-curl -o /tmp/vcredist_x64.exe $vcx64
+curl -o "/tmp/windowsdesktop-runtime-8.0.2-win-x64.exe" "$dotnet8"
+curl -o "/tmp/windowsdesktop-runtime-7.0.17-win-x64.exe" "$dotnet7"
+curl -o "/tmp/vcredist_x86.exe" "$vcx86"
+curl -o "/tmp/vcredist_x64.exe" "$vcx64"
 if [[ $wine_ver -lt 9 ]]
 then
     winetricks -q dotnet48
     winetricks -q dotnet35
     winetricks -q win10
 fi
-wine /tmp/windowsdesktop-runtime-8.0.2-win-x64.exe /passive
-wine /tmp/windowsdesktop-runtime-7.0.17-win-x64.exe /passive
-wine /tmp/vcredist_x86.exe /passive
-wine /tmp/vcredist_x64.exe /passive
-rm /tmp/vcredist_x86.exe /tmp/vcredist_x64.exe /tmp/windowsdesktop-runtime-8.0.2-win-x64.exe /tmp/windowsdesktop-runtime-7.0.17-win-x64.exe
-curl -Lso /tmp/AemulusPackageManager.7z $aemulus
-7z x /tmp/AemulusPackageManager.7z -o"${WINEPREFIX}/drive_c/"
-rm /tmp/AemulusPackageManager.7z
+wine "/tmp/windowsdesktop-runtime-8.0.2-win-x64.exe" /passive
+wine "/tmp/windowsdesktop-runtime-7.0.17-win-x64.exe" /passive
+wine "/tmp/vcredist_x86.exe" /passive
+wine "/tmp/vcredist_x64.exe" /passive
+rm "/tmp/vcredist_x86.exe" "/tmp/vcredist_x64.exe" "/tmp/windowsdesktop-runtime-8.0.2-win-x64.exe" "/tmp/windowsdesktop-runtime-7.0.17-win-x64.exe"
+curl -Lso "/tmp/AemulusPackageManager.7z" "$aemulus"
+7z x "/tmp/AemulusPackageManager.7z" -o"${WINEPREFIX}/drive_c/"
+rm "/tmp/AemulusPackageManager.7z"
 mv "${WINEPREFIX}/drive_c/AemulusPackageManagerv${version}" "${WINEPREFIX}/drive_c/AemulusPackageManager"
 
 #Setup desktop icon
-cp $SCRIPT_DIR/AemulusPackageManager.png $ICON_PATH/AemulusPackageManager.png
-sed -i "s|^Exec=.*|$EXEC_LINE|" $SCRIPT_DIR/AemulusPackageManager.desktop
-sed -i "s|^Path=.*|$PATH_LINE|" $SCRIPT_DIR/AemulusPackageManager.desktop
+cp "$SCRIPT_DIR/AemulusPackageManager.png" "$ICON_PATH/AemulusPackageManager.png"
+sed -i "s|^Exec=.*|$EXEC_LINE|" "$SCRIPT_DIR/AemulusPackageManager.desktop"
+sed -i "s|^Path=.*|$PATH_LINE|" "$SCRIPT_DIR/AemulusPackageManager.desktop"
 
-desktop-file-install --dir=$DESKTOP_PATH $SCRIPT_DIR/AemulusPackageManager.desktop
-update-desktop-database $DESKTOP_PATH
+desktop-file-install --dir="$DESKTOP_PATH" "$SCRIPT_DIR/AemulusPackageManager.desktop"
+update-desktop-database "$DESKTOP_PATH"
